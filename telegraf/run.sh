f6c7ae605d951adc8b51808195bbe3a5139df8c9
Le Provost Alexis <alexis.leprovost@deepskycorp.com>
2019-03-29 12:12:01 +0100

    remove custom proc_stat

--- telegraf/run.sh
+++ telegraf/run.sh
@@ -9,9 +9,10 @@ sed -e "s/\${TELEGRAF_HOST}/$TELEGRAF_HOST/" \
     -e "s/\${INFLUXDB_PORT}/$INFLUXDB_PORT/" \
     -e "s/\${INFLUXDB_DATABASE}/$INFLUXDB_DATABASE/" \
     $CONFIG_TEMPLATE > $CONFIG_FILE
-    
+
 hddtemp -d --listen localhost --port 7634 /dev/sd*
 
-echo "=> Starting Telegraf ..."
+mount --bind /hostfs/proc/ /proc/
 
+echo "=> Starting Telegraf ..."
 exec telegraf -config /etc/telegraf/telegraf.conf
